a5752af29f6de1cf1ba22feb136780fe289896c7
wyyllou <wyyllou@tuta.io>
2025-05-03 21:01:27 +0100

    Refactor bad tutorial code (#1)

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/f02fddb8acef29a8b32f10a335d44828d7825b78.tar.gz";
-  pkgs = import nixpkgs {
-    config = { };
-    overlays = [ ];
-  };
-in
 {
-  proxy = pkgs.callPackage ./proxy.nix { };
+  pkgs ?
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f02fddb8acef29a8b32f10a335d44828d7825b78.tar.gz")
+      { },
+}:
+pkgs.rustPlatform.buildRustPackage {
+  pname = "proxy";
+  version = "0.1.0";
+
+  src = ./.;
+
+  cargoLock.lockFile = ./Cargo.lock;
 }
