25b99722ac00acde46795d4844ce1c174f36f203
Valentine Briese <valentinegb@icloud.com>
2025-05-04 00:20:25 -0700

    Debug given password and password hash

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,13 @@ impl Authentication for HashedPasswordAuthentication {
 
     async fn authenticate(&self, credentials: Option<(String, String)>) -> Option<Self::Item> {
         credentials.and_then(|(username, password)| {
-            (username == self.username
-                && Sha256::digest(password.as_bytes())
-                    .map(|byte| format!("{byte:x}"))
-                    .join("")
-                    == self.password_hash)
+            let password_hash = Sha256::digest(password.as_bytes())
+                .map(|byte| format!("{byte:x}"))
+                .join("");
+
+            debug!(?password, ?password_hash);
+
+            (username == self.username && password_hash == self.password_hash)
                 .then_some(AuthSucceeded { username })
         })
     }
